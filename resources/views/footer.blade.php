ef2a0086caca051b8941964bb1ac04a4f4bc028c
DESKTOP-VE1MF5U\lucas <lucascamp@hotmail.com>
2018-02-16 23:34:28 -0200

    admin-lte

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="pull-right hidden-xs">
+                Hey oh, Listen what I say, oh ! oh !
+            </div>
+            <!-- Default to the left -->
+            <strong>Teste laravel VinteUm </strong> All rights reserved.
+        </footer>
+        
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+// Route::get('/', function () {
+//     return view('layout/admin_template');
+// });
+
+Route::get('/', 'TestController@index');
